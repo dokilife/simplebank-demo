f1ade2b40676562924e7bda4220bc5d25cd637cb
yokaimeow <yokaimeow@qq.com>
2023-11-04 19:30:20 +0800

    Fix: Remove unwanted fields from requests and returns

--- api/user.go
+++ api/user.go
@@ -14,13 +14,11 @@ import (
 type createUserRequest struct {
 	Username string `json:"username" binding:"required,alphanum"`
 	Password string `json:"password" binding:"required,min=6"`
-	FullName string `json:"full_name" binding:"required"`
 	Email    string `json:"email" binding:"required,email"`
 }
 
 type userResponse struct {
 	Username          string    `json:"username"`
-	FullName          string    `json:"full_name"`
 	Email             string    `json:"email"`
 	PasswordChangedAt time.Time `json:"password_changed_at"`
 	CreatedAt         time.Time `json:"created_at"`
