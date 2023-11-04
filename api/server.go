27a20ab3bd4a982f10fbfdf533c421e2da2e1164
yokaimeow <yokaimeow@qq.com>
2023-11-04 18:17:31 +0800

    Add: Why PASETO is better than JWT for token-based authentication?

--- api/server.go
+++ api/server.go
@@ -1,27 +1,46 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/dokilife/doki/db/sqlc"
+	"github.com/dokilife/doki/token"
+	"github.com/dokilife/doki/util"
 	"github.com/gin-gonic/gin"
 )
 
-// Server serves HTTP requests for our service.
+// Server serves HTTP requests for our banking service.
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and set up routing.
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
 	router := gin.Default()
 
-	router.GET("/healthz", server.healthz)
-
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 
 	server.router = router
-	return server
 }
 
 // Start runs the HTTP server on a specific address.
