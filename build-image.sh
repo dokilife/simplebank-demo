baf5f1ab941191cb73cc9cd5040591ad87ad598f
yokaimeow <yokaimeow@qq.com>
2023-11-15 17:28:16 +0800

    Add: Build an image and use Vault Secret

---
+++ build-image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TAG=1.0.0
+
+vault kv get -address https://vault.doki.life -mount=doki -format=json env | jq -r '.data.data | to_entries[] | join("=")' > app.env
+
+docker build -t harbor.doki.life/doki/doki-api:$TAG .
+
+docker push harbor.doki.life/doki/doki-api:$TAG
+
+cp -rf app.env.template app.env
